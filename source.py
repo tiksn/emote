dd4a5a492419fbac07518950c723de244c85e579
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-09-29 12:52:59 +0300

    Update source.py

--- source.py
+++ source.py
@@ -1,2 +1,19 @@
-def fetch_source(kind):
-    return f'{kind} not implemented'
+import os
+import sources
+
+def module_names():
+    folder = os.path.split(sources.__file__)[0]
+    for name in os.listdir(folder):
+        if name.endswith(".py") and not name.startswith("__"):
+            yield name[:-3]
+
+def import_sources():
+    names = list(module_names())
+    m = __import__(sources.__name__, globals(), locals(), names, 0)
+    return dict((name, getattr(m, name)) for name in names)
+
+def fetch_source(kind: str):
+    sources = import_sources()
+    source = sources[kind]
+    fetch = getattr(source, 'fetch_source')
+    return fetch()
