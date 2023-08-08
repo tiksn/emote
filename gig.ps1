e095738b1a20774d2c82d93d4f0d4227ae193d08
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-08 18:41:55 +0200

    Create gig.ps1

---
+++ gig.ps1
@@ -0,0 +1,14 @@
+#For PowerShell v3
+Function gig {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]]$list
+    )
+    $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ','
+    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params" | Select-Object -ExpandProperty content | Out-File -FilePath $(Join-Path -Path $pwd -ChildPath '.gitignore') -Encoding ascii
+}
+
+gig -list python, visualstudiocode, pycharm+all, intellij+all, powershell
+
+Add-Content -Path .\.gitignore -Value '# Repository Specific' -Encoding ascii
+Add-Content -Path .\.gitignore -Value '.idea/' -Encoding ascii
