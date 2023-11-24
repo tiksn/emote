fefb980c1f37b7c32f50e3b0840246c44ceac4d5
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-25 00:50:48 +0100

    Update source.py

--- source.py
+++ source.py
@@ -18,6 +18,14 @@ class CharacterType(Enum):
     TRITAGONIST = 3
 
 
+class CharacterSource:
+    def __init__(self, _id, short_name, long_name):
+        self.ID = _id
+        self.ShortName = short_name
+        self.LongName = long_name
+        self.Characters = []
+
+
 class Character:
     def __init__(self, _id, first_name, last_name, full_name, _type, profile_url, profile_picture_url):
         self.ID = _id
@@ -54,14 +62,17 @@ def fetch_source(kind: str):
 
     imported_sources = import_sources()
     source = imported_sources[kind]
-    source_data = {
-        "id": source.source_id,
-        "name": source.source_name
-    }
+
     fetch = getattr(source, "fetch_source")
-    characters = fetch()
-    source_data["characters"] = list(map(character_to_dict, characters))
-    pickle_data[source.source_id] = source_data
+    characters_sources = fetch()
+
+    for character_source in characters_sources:
+        source_data = {
+            "id": character_source.ID,
+            "name": character_source.LongName,
+            "characters": list(map(character_to_dict, character_source.Characters)),
+        }
+        pickle_data[character_source.ID] = source_data
 
     with open(pickle_path, 'wb') as f:
         pickle.dump(pickle_data, f, protocol=pickle.HIGHEST_PROTOCOL)
